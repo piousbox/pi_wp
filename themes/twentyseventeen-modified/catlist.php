9e86160a435540faf55dcad01a8024d64b1e1652
Victor Pudeyev <piousbox@gmail.com>
2018-09-17 08:04:27 -0500

    images, and categorization

--- themes/twentyseventeen-modified/catlist.php
+++ themes/twentyseventeen-modified/catlist.php
@@ -1,16 +1,48 @@
 <h2>All Categories</h2>
 <?php
+  function fn($data) { return $data; } $fn = 'fn';
+
   $parent = get_category_by_slug('technique');
   // var_dump( $parent );
+
+  $category = get_category( get_query_var( 'cat' ) );
   $args = array(
-    'child_of'            => $parent->term_id,
+    'parent'            => $category->term_id,
     'hierarchical'        => true,
     'order'               => 'ASC',
     'orderby'             => 'name',
     'show_count'          => 1,
     'use_desc_for_title'  => 1,
   );
+
+  $categories = get_categories( $args ); // and all children
+
+  $all_args = array(
+    'child_of'            => $category->term_id,
+    'hierarchical'        => true,
+    'order'               => 'ASC',
+    'orderby'             => 'name',
+    'show_count'          => 1,
+    'use_desc_for_title'  => 1,
+  );
+  $all_categories = get_categories( $all_args );
+
+  $tmpl = '';
+  foreach($categories as $cat) {
+    $img = z_taxonomy_image_url($cat->term_id);
+    $tmp =<<<EOT
+<div class="item">
+  <h5>$cat->name</h5>
+  <img src="$img" alt="" />
+</div>
+EOT;
+   $tmpl = $tmpl . $tmp; 
+    // echo z_taxonomy_image($cat->term_id); 
+  }
+
+  echo("<div class='items'>$tmpl</div>");
+
   echo "<ul>";
-  echo wp_list_categories( $args );
+  echo wp_list_categories( $all_args );
   echo "</ul>";
 ?>
